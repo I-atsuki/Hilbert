625b614ba8c68c98aa05daca87ddec2b2374d201
ikedaatsuki <thehottesttree@gmail.com>
2018-04-20 15:47:41 +0900

    4-20 15:47

--- app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/hilbert/MainActivity.java
@@ -12,11 +12,17 @@ public class MainActivity extends AppCompatActivity {
     private Button decButton, incButton;
     private final static int MAX_ORDER = 9;
     private int order = 1;
+    private final static String KEY_NAME = "MainActivity.order";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null)
+            order = savedInstanceState.getInt(KEY_NAME);
+
+
         orderView = findViewById(R.id.order_view);
         hilbertView = findViewById(R.id.hilbert_view);
         decButton = findViewById(R.id.dec_button);
@@ -54,5 +60,12 @@ public class MainActivity extends AppCompatActivity {
         hilbertView.setOrder(order);
         decButton.setEnabled(order > 1);
         incButton.setEnabled(order < MAX_ORDER);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(KEY_NAME, order);
     }
 }
